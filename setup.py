1d7cbe406cd8dcf7637e8d13d491ba6699d12a89
Mattias Franberg <mattias.franberg@gmail.com>
2013-04-03 15:48:58 +0200

    Added setup.py script and associated files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="svsim",
+    version="0.1dev",
+    package_dir={ "svsim" : "svsim" },
+    license="Modified BSD",
+    long_description = open( "README.md", "r" ).read( ),
+    url="http://www.example.com/",
+    maintainer="The svest team.",
+    maintainer_email="mattias.franberg@scilifelab.se"
+)
