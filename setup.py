93864ee8608da356aa8c9e22c733474b33f5f7c8
Mattias Franberg <mattias.franberg@gmail.com>
2013-08-22 13:43:40 +0200

    Calling python setup.py install did not install any packages or data. This commit fixes this by including the package and package_data arguments.

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(
     name="svsim",
     version="0.1dev",
-    package_dir={ "svsim" : "svsim" },
+    package_dir = { "svsim" : "svsim" },
+    packages = ['svsim', 'svsim.reads'],
+    package_data= { 'svsim': [ 'data/*.mconf' ] },
     license="Modified BSD",
     long_description = open( "README.md", "r" ).read( ),
     url="http://www.example.com/",
