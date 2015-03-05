e1455c9d4fdc2142e881c5887cee320639131714
Mans Magnusson <monsunas@gmail.com>
2015-03-05 12:54:20 +0100

    Can simulate multiple contigs of reference genome

--- svsim/commands/simulate_genome.py
+++ svsim/commands/simulate_genome.py
@@ -6,14 +6,17 @@ import random
 
 import click
 
-##
-# Generates base pairs according to the given
-# distribution.
-#
-# @param probs A list of probabilities for each base pair.
-# @param bp A list of base pairs.
-#
 def generate_bp(probs, bp):
+    """
+    Generates base pairs according to the given distribution.
+    
+    Arguments:
+        probs (list): Probabilities for each base pair.
+        bp (list): A list of base pairs.
+    
+    Returns:
+        base_pair (str): The generated basepair
+    """
     rand = random.random( )
     total = 0.0
     for i in range( len( probs ) ):
@@ -36,45 +39,83 @@ def genome_generator(probabilities, length):
     for i in range( length ):
         yield generate_bp( probabilities, bp )
 
-##
-# Writes the genome to the given output file in
-# fasta format.
-#
-# @param genome Sequence of the genome to write.
-# @param output_file Output file to write the genome to.
-#
-
-def write_genome(genome_generator, output_file):
-    output_file.write( ">normal-genome\n" )
-    for base in genome_generator:
-        output_file.write( base )
-    output_file.write( '\n' )
+def write_genome(output_file, contigs, genome_name, probabilities):
+    """
+    Writes the genome to the given output file in fasta format.
+    
+    Arguments:
+        output_file (file_handle): Output file to write the genome to.
+        contigs (dict): A dictionary with {<contig_name>: <contig_length>}
+        genome_name (str): A string that represents the genome name
+        probabilities (list): A list with the probabilities for each position. 
+    """
+    for contig in contigs:
+        contig_length = contigs[contig]
+        output_file.write(">{0} dna:chromosome chromosome:{1}:{0}:1:{2}:1 REF\n".format(
+                                contig,
+                                genome_name,
+                                contig_length
+                                )
+                            )
+        for base in genome_generator(probabilities, contig_length):
+            output_file.write( base )
+        output_file.write( '\n' )
+    return
 
 
 @click.command()
-@click.argument('length',
-                    type=int
-)
 @click.argument('output',
                     type=click.File('wb'),
 )
-@click.option('-f', '--frequencies',
+@click.option('-p', '--probabilities',
                     nargs=4,
                     type=float,
                     default=[0.25,0.25,0.25,0.25],
-                    help="List of frequencies for A, C, G and T respectively."
+                    help="List of probabilities for A, C, G and T respectively."
 )
-def simulate_genome(length, output, frequencies):
+@click.option('-n', '--genome_name',
+                    default='normal-genome',
+                    help="Add a contig that should be included."
+)
+
+@click.option('-c', '--contig',
+                    multiple=True,
+                    default=['1'],
+                    help="Add a contig that should be included."
+)
+@click.option('-l','--contig_length',
+                    multiple=True,
+                    default=[1000],
+                    help=("Specify the length of the contig"
+                        "(in same order as the contigs are given).")
+                    
+)
+def simulate_genome(output, probabilities, genome_name, contig, contig_length):
     """
-    Simulates a genome from the given base probabilities..
+    Simulates a fasta genome from the given base probabilities.
+    
+    User can specify multiple contigs by using -c/--contig.
+    Each contig needs to have a length specified (use -l/--contig_length).
+    genome_name is the name of the genome (default normal-genome).
+    
+    Each contig will get a header line in the fasta file that looks like:
+    ><contig> dna:chromosome chromosome:<genome_name>:<contig>:<start>:<stop>:1 REF
     """
     try:
-        assert( abs( sum( frequencies ) - 1.0 ) <= 0.001 )
+        assert( abs( sum( probabilities ) - 1.0 ) <= 0.001 )
     except AssertionError:
-        print('The sum of the frequencies must equal 1', file=sys.stderr)
+        print('The sum of the probabilities must equal 1', file=sys.stderr)
         sys.exit()
-        
-    write_genome( genome_generator( frequencies, length ), output )
+    try:
+        assert( len( contig ) == len( contig_length) )
+    except AssertionError:
+        print('Each contig must have a length', file=sys.stderr)
+        sys.exit()
+    contigs = {}
+    for i, contig in enumerate(contig):
+        contigs[contig] = int(contig_length[i])
+    
+    write_genome( output, contigs, genome_name, probabilities)
     
 
 if __name__ == '__main__':
