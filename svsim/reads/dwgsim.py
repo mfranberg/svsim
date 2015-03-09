cc1c76cfb81f28603a35c5a38c3e1b32045551a0
Mans Magnusson <monsunas@gmail.com>
2015-03-09 13:13:24 +0100

    Removed dwgsimulator file

--- svsim/reads/dwgsim.py
+++
@@ -1,3 +0,0 @@
-
-from svsim.reads.isim import IReadSimulator
-
--- svsim/reads/simulator.py
+++ svsim/reads/simulator.py
@@ -1,14 +1,17 @@
-##
-# Abstract base class for read simulators.
-#
-#
-class IReadSimulator:
+import os
+import subprocess
+import tempfile
+
+class IReadSimulator(object):
+    """
+    Abstract base class for read simulators.
+    """
     def __init__(self):
+        super(IReadSimulator, self).__init__()
         ##
         # Genome coverage.
         #
         self.coverage = 10
-
         ##
         # Read length.
         #
@@ -17,19 +20,54 @@ class IReadSimulator:
         ##
         # Mean distance between read pairs.
         #
+        
         self.mean = 500
-
         ##
         # Standard deviation for distance between pairs.
         # 
         self.std = 50
-
-    ##
-    # Simulates reads from the given genome and writes
-    # them in the given output file.
-    #
-    # @param genome_path File that contains a genome to simulate reads from.
-    # @param output_path File to write simulated reads to.
-    #
+    
     def simulate(genome_path, output_path):
-        raise Exception( "ReadSimulator: Unimplemented abstract method." )
+        """
+        Simulates reads from the given genome and writes them in the 
+        given output file.
+        
+        Arguments:
+            genome_path (str): Path to file that contains a genome to simulate 
+                                reads from.
+            output_path (str): Path to file to write simulated reads to.
+        """
+        raise NotImplementedError( "ReadSimulator: Unimplemented abstract method." )
+
+
+class DwgsimSimulator(IReadSimulator):
+    """Simulates reads using dwgsim with Illumina-like properties."""
+    def __init__(self):
+        super(DwgsimSimulator, self).__init__()
+    
+    def simulate(self, genome_path, output_file):
+        output_prefix = os.path.join( tempfile.mkdtemp( ), "dwgsim" )
+
+        read_error_rate = "{0}-{0}".format( self.read_error )
+        subprocess.check_call(
+            [ 
+                "dwgsim",
+                "-d", str( int( self.mean ) ),
+                "-s", str( int( self.std ) ),
+                "-C", str( self.coverage ),
+                "-1", str( int( self.read_length ) ),
+                "-2", str( int( self.read_length ) ),
+                "-e", read_error_rate,
+                "-E", read_error_rate,
+                "-c", "0",
+                "-r", "0",
+                "-R", "0",
+                "-y", "0",
+                genome_path,
+                output_prefix 
+            ], 
+            stdout = open( "/dev/null" ), 
+            stderr = open( "/dev/null" ) )
+        
+        subprocess.check_call( [ "cp", output_prefix + ".bwa.read1.fastq", output_file + "_pe1.fa" ] )
+        subprocess.check_call( [ "cp", output_prefix + ".bwa.read2.fastq", output_file + "_pe2.fa" ] )
