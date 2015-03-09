f951ea5aebbd1bd53fb6f889cf91ed0eb7a72031
Mans Magnusson <monsunas@gmail.com>
2015-03-09 13:24:34 +0100

    Moved MetaSim to simulator.py

--- svsim/reads/simulator.py
+++ svsim/reads/simulator.py
@@ -1,6 +1,13 @@
 import os
 import subprocess
 import tempfile
+import fnmatch
+import logging
+
+from pkg_resources import resource_filename
+
+import svsim.log as log
+from svsim.util import calculate_num_reads, get_genome_length
 
 class IReadSimulator(object):
     """
@@ -46,17 +53,17 @@ class DwgsimSimulator(IReadSimulator):
         super(DwgsimSimulator, self).__init__()
     
     def simulate(self, genome_path, output_file):
-        output_prefix = os.path.join( tempfile.mkdtemp( ), "dwgsim" )
+        output_prefix = os.path.join(tempfile.mkdtemp(), "dwgsim")
 
-        read_error_rate = "{0}-{0}".format( self.read_error )
+        read_error_rate = "{0}-{0}".format(self.read_error)
         subprocess.check_call(
             [ 
                 "dwgsim",
-                "-d", str( int( self.mean ) ),
-                "-s", str( int( self.std ) ),
-                "-C", str( self.coverage ),
-                "-1", str( int( self.read_length ) ),
-                "-2", str( int( self.read_length ) ),
+                "-d", str(int(self.mean)),
+                "-s", str(int( self.std)),
+                "-C", str(self.coverage),
+                "-1", str(int(self.read_length)),
+                "-2", str(int(self.read_length)),
                 "-e", read_error_rate,
                 "-E", read_error_rate,
                 "-c", "0",
@@ -66,8 +73,116 @@ class DwgsimSimulator(IReadSimulator):
                 genome_path,
                 output_prefix 
             ], 
-            stdout = open( "/dev/null" ), 
-            stderr = open( "/dev/null" ) )
+            stdout = open("/dev/null"), 
+            stderr = open("/dev/null"))
         
-        subprocess.check_call( [ "cp", output_prefix + ".bwa.read1.fastq", output_file + "_pe1.fa" ] )
-        subprocess.check_call( [ "cp", output_prefix + ".bwa.read2.fastq", output_file + "_pe2.fa" ] )
+        subprocess.check_call(
+            [
+                "cp", 
+                output_prefix + ".bwa.read1.fastq", 
+                output_file + "_pe1.fa"
+            ]
+        )
+        subprocess.check_call(
+            [
+                "cp", 
+                output_prefix + ".bwa.read2.fastq", 
+                output_file + "_pe2.fa"
+            ]
+        )
+
+class MetaSimSimulator(IReadSimulator):
+    """Simulates reads using metasim."""
+    def __init__(self):
+        super(MetaSimSimulator, self).__init__()
+        self.error_model = resource_filename( "svsim", "data/errormodel-100bp.mconf" )
+    
+    def simulate(self, genome_path, output_file):
+        if not self.read_length == 100:
+            raise ValueError("Read length must be 100 for metasim.")
+        
+        genome_length = get_genome_length(genome_path)
+        num_reads = calculate_num_reads(self.coverage, self.read_length, genome_length)
+        output_dir = tempfile.mkdtemp()
+        
+        logging.info( "Starting metasim:" )
+        subprocess.check_call(
+            [
+                 "MetaSim", 
+                 "cmd",
+                 "-r", 
+                 str(num_reads), 
+                 "-m",
+                 "-g", 
+                 self.error_model,
+                 "-2", 
+                 self.error_model,
+                 "--empirical-pe-probability", 
+                 "100",
+                 "--clones-mean", 
+                 str(self.mean),
+                "--clones-param2", 
+                str(self.std),
+                "-d", 
+                output_dir,
+                genome_path 
+            ], 
+            stdout = log.get_log_stream()
+        )
+        
+        # Metasim outputs reads for each contig, gather them into one file
+        found_match = False
+        for i, metasim_output_file in enumerate(os.listdir(output_dir)):
+            metasim_output_name = os.path.basename(genome_path).split(".")[0] + "-Empirical.*fna"
+            if fnmatch.fnmatch(metasim_output_file, metasim_output_name):
+                metasim_output_file_absolute = os.path.join(output_dir, metasim_output_file)
+                convert_to_pe(
+                                metasim_output_file_absolute, 
+                                output_file + "_pe1.fa", 
+                                output_file + "_pe2.fa", 
+                                append = i > 0
+                            )
+                found_match = True
+        
+        if not found_match:
+            raise RuntimeError( "MetaSim could not simulate reads, is the genome too short?" )
+
+def convert_to_pe(metasim_output_path, pe1_path, pe2_path, append = False ):
+    """
+    Converts a metasim output fasta file in which all reads are
+    in one file, to two separate files containg the first and second
+    pair respectively.
+    
+    Arguments:
+        metasim_output_path (str): An output file from metasim.
+        pe1_path (str): The path to fasta file that will contain the first read.
+        pe2_path (str): The path to fasta file that will contain the second read.
+        append (bool): Determines whether to append to the output files instead.
+    """
+    open_type = "w"
+    if append:
+        open_type = "a"
+    
+    output_pe1 = open(pe1_path, open_type)
+    output_pe2 = open(pe2_path, open_type)
+    output_file = None
+    
+    with open(metasim_output_path, "r") as metasim_output_file:
+        for line in metasim_output_file:
+            columns = line.split()
+            
+            if columns[0].startswith(">"):
+                if output_file:
+                    output_file.write("\n")
+                
+                if columns[0].endswith(".1"):
+                    output_file = output_pe1
+                else:
+                    output_file = output_pe2
+                
+                output_file.write(line)
+            else:
+                output_file.write(line.strip())
+    
+    output_pe1.close()
+    output_pe2.close()
