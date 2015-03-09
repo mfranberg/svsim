ed95dbce75359c79346a72c30928e122f843a09f
Mans Magnusson <monsunas@gmail.com>
2015-03-09 11:28:22 +0100

    Updated to loogbok and PEP8 in map_reads

--- svsim/__init__.py
+++ svsim/__init__.py
@@ -1,2 +1,3 @@
 from svsim.variation import (read_variations, create_sv, check_variations,
                              write_donor_contigs)
+from svsim.log import init_log
--- svsim/commands/map_reads.py
+++ svsim/commands/map_reads.py
@@ -11,29 +11,25 @@ import tempfile
 
 import pysam
 
-import svsim.log as log
+from logbook import Logger
 
-##
-# Converts a sam file to a bam file using pysam.
-#
-# @param sam_path Path of the .sam file.
-# @param bam_path Path of the resulting .bam file.
-#
+from svsim import init_log
+
+logger = Logger('map_reads logger')
+
 def sam_to_bam(sam_path, bam_path):
-    sam_file = pysam.AlignmentFile( sam_path, "r" )
-    bam_file = pysam.AlignmentFile( bam_path, "wb", template = sam_file )
+    """
+    Converts a sam file to a bam file using pysam.
+    
+    Arguments:
+        sam_path (str): Path of the .sam file.
+        bam_path (str): Path of the resulting .bam file.
+    """
+    sam_file = pysam.AlignmentFile(sam_path, "r")
+    bam_file = pysam.AlignmentFile(bam_path, "wb", template = sam_file)
     for alignment in sam_file:
-        bam_file.write( alignment )
+        bam_file.write(alignment)
 
-##
-# Maps the given paired end reads using bwa, and writes a
-# sorted .bam file in the given output file.
-#
-# @param pe1_path Path of the first reads.
-# @param pe2_path Path of the second reads.
-# @param genome_path Path to the reference genome.
-# @param output_path Path of the output file without extension ".bam".
-#
 @click.command()
 @click.argument('genome',
                     type=click.Path(exists=True),
@@ -51,48 +47,53 @@ def sam_to_bam(sam_path, bam_path):
 )
 @click.option('-l', '--logfile',
                     type=click.Path(exists=False),
-                    default='./svsim_log.txt',
-                    help='Path to log file.'
+                    help="Path to log file. If none logging is "\
+                          "printed to stderr."
 )
 def map_reads(pe1_path, pe2_path, genome, output, logfile):
     """
-    Map reads to a given genome.
+    Maps the given paired end reads using bwa, and writes a sorted .bam file 
+    in the given output file.
+    
+    genome: Path to the reference genome
+    output: Path of the output file without extension ".bam".
     """
-    log.init_log( logfile )
+    
+    log = init_log(logfile)
+    log.push_application()
+    
     if not (pe1_path and pe1_path):
-        print('Please provide paths to the read files with -pe1 and -pe2', file=sys.stderr)
-        sys.exit()
+        logger.error('Please provide paths to the read files with -pe1 and -pe2')
+        sys.exit(1)
     
     
-    work_dir = tempfile.mkdtemp( )
-    genome_db = os.path.join( work_dir, "genome" )
-    pe1_output = os.path.join( work_dir, "pe1.sai" )
-    pe2_output = os.path.join( work_dir, "pe2.sai" )
-    bwa_output = os.path.join( work_dir, "output.sam" )
+    work_dir = tempfile.mkdtemp()
+    genome_db = os.path.join(work_dir, "genome")
+    pe1_output = os.path.join(work_dir, "pe1.sai")
+    pe2_output = os.path.join(work_dir, "pe2.sai")
+    bwa_output = os.path.join(work_dir, "output.sam")
+        
+    logger.info("Running bwa index, aln and sampe in {0}".format(work_dir))
     
-    log_file = log.get_log_stream( )
+    subprocess.check_call(["bwa", "index", "-p", genome_db, genome], stderr = log_file)
     
-    logging.info( "Running bwa index, aln and sampe in {0}".format( work_dir ) )
+    with open(pe1_output, "w") as pe1_file:
+        subprocess.check_call(["bwa", "aln", genome_db, pe1_path], stdout = pe1_file, stderr = log_file)
     
-    subprocess.check_call( [ "bwa", "index", "-p", genome_db, genome ], stderr = log_file )
+    with open(pe2_output, "w") as pe2_file:
+        subprocess.check_call(["bwa", "aln", genome_db, pe2_path], stdout = pe2_file, stderr = log_file)
     
-    with open( pe1_output, "w" ) as pe1_file:
-        subprocess.check_call( [ "bwa", "aln", genome_db, pe1_path ], stdout = pe1_file, stderr = log_file )
-    
-    with open( pe2_output, "w" ) as pe2_file:
-        subprocess.check_call( [ "bwa", "aln", genome_db, pe2_path ], stdout = pe2_file, stderr = log_file )
-    
-    with open( bwa_output, "w" ) as bwa_file:
-        subprocess.check_call( [ "bwa", "sampe",
+    with open(bwa_output, "w") as bwa_file:
+        subprocess.check_call(["bwa", "sampe",
                            "-r", "@RG\tID:ILLUMINA\tSM:48_2\tPL:ILLUMINA\tLB:LIB1",
                            genome_db,
                            pe1_output, pe2_output,
-                           pe1_path, pe2_path ], stdout = bwa_file, stderr = log_file )
+                           pe1_path, pe2_path], stdout = bwa_file, stderr = log_file)
     
-    logging.info( "Sorting .bam file" )
+    logger.info("Sorting .bam file")
     
-    sam_to_bam( bwa_output, bwa_output + ".bam" )
-    pysam.sort( bwa_output + ".bam", output )
+    sam_to_bam(bwa_output, bwa_output + ".bam")
+    pysam.sort(bwa_output + ".bam", output)
 
 if __name__ == '__main__':
     map_reads()
